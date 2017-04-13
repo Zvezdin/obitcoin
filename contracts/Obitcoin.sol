0490e83cc40edef5499c6ca0e1fefbcc647b378f
Zvezdin <git@besarabov.com>
2017-04-14 01:24:18 +0300

    Started splitting the contract on libraries

--- contracts/Obitcoin.sol
+++ contracts/Obitcoin.sol
@@ -1,20 +1,15 @@
 pragma solidity ^0.4.9;
 
+import "BonusPools";
+import "MembersLib";
+
 contract Obitcoin{
+    using BonusPools for BonusPools.Pool;
+    using MembersLib for MembersLib.Member;
     
     //events for all actions made by the contract. They are fired by the contract, then catched and processed by the web application
     //once an event is fired, it will always stay in the blockchain. This is the why our application can load all transactions that ever occurred within the contracy
     //the contract has no access to previously fired events. That's why firing events is always cheaper than saving the same amount of data in contract storage
-    event TokenTransfer(uint16 indexed from, uint16 indexed to, uint16 indexed pool, int256 amount, uint time); //int256 because we can both remove and add tokens
-    
-    event MoneyTransfer(uint16 indexed from, uint16 indexed to, uint16 indexed pool, int256 amount, uint time);
-    
-    event SliceTransfer(uint16 indexed from, uint16 indexed to, uint16 indexed pool, int256 amount, uint time); //uint128 because we can only add slices
-    
-    event TokenPurchase(uint16 indexed from, uint16 indexed pool, uint128 amount, uint time);
-    
-	//event for the creaition of a pool, containing it's index for later access
-    event PoolChanged(uint16 indexed from, uint16 indexed pool, bool added, uint time);
 	
 	//event to notify that someone has tried to access a method that he shouldn't have
 	event UnauthorizedAccess(uint16 indexed from, address indexed fromAddress, uint time);
@@ -30,33 +25,10 @@ contract Obitcoin{
 	event Voted(uint16 indexed from, uint16 indexed voteIndex, uint16 pool, bool vote, uint time);
 	
 	event Delegation(uint16 indexed from, uint16 indexed to, uint16 indexed pool, uint time);
-	
-	
-    enum PermLevel { none, member, admin, owner } //permission levels of contract members
+
 
     enum VoteType { ParameterChange, TokenIssue, PullRequest } //shows the type of a certain vote action
     enum VoteState { Pending, Successful, Unsuccessful } //if the voting is collecting votes, has finished successfully or unsuccessfully
-
-	struct Member { //the struct of a contract (and cooperative) member
-	    bytes32 name; //maximum name length = 32 bytes. This is to avoid dynamic string, which is resource heavy.
-	    PermLevel permLevel; //current permission level. Can be changed to everything but owner.
-	    bool exists; //used to check if this struct is initialized
-	    address adr; //current address of the person. Can be changed.
-	}
-	//Each contract member has an ID of type uint16
-	
-    struct Pool { //the struct of a cooperative debt pool (containing members working on a certain action)
-        uint16[] members; //memberIds
-        mapping (uint16 => uint128[3]) balance; //the balance of each member
-        //uint[0] = tokens, uint[1] = slices, uint[2] = balance
-        mapping (uint16 => uint16) delegations; //who to who is delegating his voting rights
-        uint128[3] totalBalance; //sum of balance for all pool members
-        bool exists;
-        bytes16 name;
-        bytes16 legalContract;
-        bytes16 financialReports;
-    }
-    //Each Pool has an ID of type uint16
     
     //A vote struct that indicates a certain action needs to be voted on to be successfully executed
     struct Vote {
@@ -78,10 +50,10 @@ contract Obitcoin{
     
     Vote[] activeVotes;
     
-    mapping(uint16 => Member) members;
+    mapping(uint16 => MembersLib.Member) members;
     uint16[] memberIds;
     
-    mapping(uint16 => Pool) pools;
+    mapping(uint16 => BonusPools.Pool) pools;
     uint16[] poolIds;
     
     mapping(address => uint16) memberAddresses; //to access the member id from his current address. Must be updated if a member's address changes
@@ -106,9 +78,9 @@ contract Obitcoin{
     
     function init(){
         if(owner != msg.sender || members[memberAddresses[owner]].exists) throw;
-        members[memberAddresses[owner]].permLevel = PermLevel.owner;
+        members[memberAddresses[owner]].permLevel = MembersLib.PermLevel.owner;
         updateMember(0, "Coordinator", owner, true);
-        members[memberAddresses[owner]].permLevel = PermLevel.owner;
+        members[memberAddresses[owner]].permLevel = MembersLib.PermLevel.owner;
         
         updateMember(0, "Pesho", 0x2e9Cf98A103148172B4e773F0ed2C58269A3fbd6, false);
         updateMember(0, "Georgi", 0xa0323703351Bc5e4905A5080d0f1e3fc4e57220f, false);
@@ -148,7 +120,7 @@ contract Obitcoin{
         sendTokens(1, toMembers, amount);
     }
     
-    modifier only(PermLevel level){ //only a person with the same or higher permission level is allowed to call the function
+    modifier only(MembersLib.PermLevel level){ //only a person with the same or higher permission level is allowed to call the function
         if(members[memberAddresses[msg.sender]].permLevel >= level){
             _;
             return;
@@ -168,9 +140,9 @@ contract Obitcoin{
         _;
     }*/
     
-    function updateMember(uint16 member, bytes32 name, address adr, bool isAdmin) public only(PermLevel.owner) {
+    function updateMember(uint16 member, bytes32 name, address adr, bool isAdmin) public only(MembersLib.PermLevel.owner) {
         if(member != 0 && !members[member].exists) throw;
-        if(member != 0 && members[member].permLevel == PermLevel.owner) throw; //the owner should stay untouched
+        if(member != 0 && members[member].permLevel == MembersLib.PermLevel.owner) throw; //the owner should stay untouched
         if(memberAddresses[adr] != member && memberAddresses[adr] != 0) throw; //can't add this person if the same address is already in the system.
         
         bool added = member == 0;
@@ -185,31 +157,22 @@ contract Obitcoin{
             memberAddresses[adr] = member;
         }
         
-        if(members[member].permLevel == PermLevel.admin != isAdmin){
+        if(members[member].permLevel == MembersLib.PermLevel.admin != isAdmin){
             AdminChanged(memberAddresses[msg.sender], member, isAdmin, now);
         }
         
-        members[member] = Member({name: name, adr: adr, permLevel: isAdmin ? PermLevel.admin : PermLevel.member, exists: true});
+        members[member] = MembersLib.Member({name: name, adr: adr, permLevel: isAdmin ? MembersLib.PermLevel.admin : MembersLib.PermLevel.member, exists: true});
         
         PersonChanged(memberAddresses[msg.sender], member, added, now);
     }
     
-    function updateDebtPool(uint16 pool, bytes16 name, bytes16 legalContract, bytes16 financialReports) public only(PermLevel.admin) {
-        if(pool != 0 && !pools[pool].exists) throw;
-        
-        bool added = pool == 0;
-        
-        if(added){ //create the pool
+    function updateDebtPool(uint16 pool, bytes16 name, bytes16 legalContract, bytes16 financialReports) public only(MembersLib.PermLevel.admin) {
+        if(pool == 0){ //we need to add a pool, register the new ID
             pool = poolCounter++;
             poolIds.push(pool);
         }
         
-        pools[pool].name = name;
-        pools[pool].legalContract = legalContract;
-        pools[pool].financialReports = financialReports;
-        pools[pool].exists = true;
-        
-        PoolChanged(memberAddresses[msg.sender], pool, added, now);
+        pools[pool].updateDebtPool(pool, name, legalContract, financialReports, memberAddresses[msg.sender]);
     }
 	
 	function getContractAddress() public constant returns (address){ //this method is used to test if the web application is connecting to an actual contract
@@ -225,31 +188,13 @@ contract Obitcoin{
 	}
 	
 	function getPool(uint16 poolId) public constant returns (bytes16[3], uint16[], uint16[], uint128[3], uint128[3][]){
-	    Pool pool = pools[poolId];
-	    
-	    if(!pool.exists) throw;
-	    
-	    
-	    bytes16[3] memory data;
-	    uint128[3][] memory membersBalance = new uint128[3][](pool.members.length);
-	    uint16[] memory delegations = new uint16[](pool.members.length);
-	    
-	    data[0] = pool.name;
-	    data[1] = pool.legalContract;
-	    data[2] = pool.financialReports;
-	    
-	    for(uint16 i = 0; i<pool.members.length; i++){
-	        membersBalance[i] = pool.balance[pool.members[i]];
-	        delegations[i] = pool.delegations[pool.members[i]];
-	    }
-	    
-	    return(data, pool.members, delegations, pool.totalBalance, membersBalance);
+	    return pools[poolId].getPool();
 	}
     
-    function getMembers() public constant returns (uint16[], bytes32[], address[], PermLevel[]){
+    function getMembers() public constant returns (uint16[], bytes32[], address[], MembersLib.PermLevel[]){
         bytes32[] memory names = new bytes32[](memberIds.length);
         address[] memory addresses = new address[](memberIds.length);
-        PermLevel[] memory permLevels = new PermLevel[](memberIds.length);
+        MembersLib.PermLevel[] memory permLevels = new MembersLib.PermLevel[](memberIds.length);
         
         for(uint16 i = 0 ; i<memberIds.length; i++){
             names[i] = members[memberIds[i]].name;
@@ -277,9 +222,9 @@ contract Obitcoin{
     }
     
     //TODO check if the "to" is a valid member
-    function delegateVote(uint16 poolId, uint16 to) public only(PermLevel.member){
+    function delegateVote(uint16 poolId, uint16 to) public only(MembersLib.PermLevel.member){
         uint16 member = memberAddresses[msg.sender]; //get the ID of the caller
-        Pool pool = pools[poolId];
+        BonusPools.Pool pool = pools[poolId];
         
         if(to != 0){
             if(pool.balance[to][1] == 0) throw; //if 'to' is not a member of the pool (aka has no slices in it)
@@ -306,7 +251,7 @@ contract Obitcoin{
     function vote(uint16 voteIndex, bool voteFor) public onlyPoolMember(activeVotes[voteIndex].pool){
         Vote vote = activeVotes[voteIndex];
         
-        Pool pool = pools[vote.pool];
+        BonusPools.Pool pool = pools[vote.pool];
         
         uint16 member = memberAddresses[msg.sender];
         
@@ -356,7 +301,7 @@ contract Obitcoin{
         
     }
     
-    function sendTokens(uint16 pool, uint16[] toMembers, int[] amount) public only(PermLevel.owner) {
+    function sendTokens(uint16 pool, uint16[] toMembers, int[] amount) public only(MembersLib.PermLevel.owner) {
         //check to see if the data is valid. The function sendTokens will further check if every pool and member exists.
         if(toMembers.length == 0 || amount.length == 0 || amount.length != toMembers.length) throw;
         if(!pools[pool].exists) throw;
@@ -387,126 +332,11 @@ contract Obitcoin{
     
     //possible bad situation: when voting is successful and this method is executed, in case it fails it leaves the voting to be "pending" forever.
     function executeTokenSending(uint16 pool, uint16[] toMembers, int[] totalAmount) private{
-        if(totalAmount.length != toMembers.length) throw;
-        
-        uint16 member;
-        int amount;
-        uint128[3] balance;
-        
-        for(uint16 i=0; i< toMembers.length; i++){
-            member = toMembers[i];
-            balance = pools[pool].balance[member];
-            amount = totalAmount[i];
-            
-            if(balance[0] + amount < balance[0]) throw; //avoid the 16-byte INT overflow
-            if(balance[1] + amount < balance[1]) throw;
-            if(-amount > balance[0]) throw; //if we'll have to subtract more tokens than available
-            
-            if(balance[0] == 0 && balance[1]==0) pools[pool].members.push(member); //if the member is not a member of the pool, add him
-            
-            if(amount>0){
-                balance[0] += uint128(amount);
-                balance[1] += uint128(amount);
-                
-                pools[pool].totalBalance[0] += uint128(amount);
-                pools[pool].totalBalance[1] += uint128(amount);
-            } else {
-                balance[0] -= uint128(-amount);
-                balance[1] -= uint128(-amount);
-                
-                pools[pool].totalBalance[0] -= uint128(-amount);
-                pools[pool].totalBalance[1] -= uint128(-amount);
-            }
-            
-            TokenTransfer(memberAddresses[msg.sender], member, pool, amount, now); //add to the current amount of tokens of the person
-            SliceTransfer(memberAddresses[msg.sender], member, pool, amount, now); //add to the total amount of tokens of the person
-        }
+        pools[pool].executeTokenSending(pool, toMembers, totalAmount, memberAddresses[msg.sender]);
     }
     
     //this function uses the most gas. Split it on multiple calls to compensate for that. Perhaps make a call to split money for the first x members, then for the next x...
-    function buyTokens(uint16 pool, uint128 amount) public only(PermLevel.admin) {
-        Pool debtPool = pools[pool];
-        
-        if(amount == 0 || !debtPool.exists || debtPool.members.length==0) throw;
-        
-        
-        //memory, because the array is copied and not referenced (which we want, because writing to storage is expensive)
-        uint128[3] memory sum = debtPool.totalBalance; //get the token, money and slice sums. In an array because of the stack depth limit
-        uint128 totalSent = 0; //tracking how much is actually sent.
-		
-		uint i=0;
-		
-		if(sum[1]==0) throw; //we cannot split if we don't have any slices to split based on!
-		
-		uint16 member;
-		uint128 share;
-		uint128 value;
-		
-		uint128 leftToSplit;
-		
-	    int256[] memory moneyToApply = new int256[](pools[pool].members.length); //to avoid additional writes to storage, we're doing everything on memory and applying it afterwards in storage
-		int256[] memory tokensToApply = new int256[](pools[pool].members.length);
-		
-		bool stage1 = sum[0] > 0;
-		    
-	    while(amount>totalSent){ //ideally the loop should terminate after 1 to 3 iterations
-	        leftToSplit = amount - totalSent; //what is left to split for this iteration of the loop
-			
-    		for(i=0; i<debtPool.members.length; i++){
-    		    member = debtPool.members[i];
-    		    
-    			share = stage1 ? debtPool.balance[member][0] : debtPool.balance[member][1]; //get the tokens or slices of that person
-    			
-    			value = (share*leftToSplit)/(stage1 ? debtPool.totalBalance[0] : sum[1]); //get the value that we're going to send to the member. Either buying coins or sending money directly. We're reading the token sum from there, because our local one is being modified constantly
-    			
-    			if(i == debtPool.members.length-1 && (!stage1 || amount-totalSent <= sum[0]) ){ //if we're at the last iteration of the main and inner loop
-    			    value = amount-totalSent; //transfer what's left from integer arithmetics inaccuracies
-    			}
-    			
-    			
-    			if(tokensToApply[i] == 0 && moneyToApply[i] == 0){ //copy the member's balance to memory if we haven't already
-    			    tokensToApply[i] = debtPool.balance[member][0]; //this is done to avoid expensive excessive storage I/O
-    			    moneyToApply[i] = debtPool.balance[member][2];
-    			}
-    			
-    			if(value == 0){
-                    continue; //no need to waste gas if we're not doing anything
-    			}
-    			
-				if(stage1){ //if we're buying tokens
-				    if(value>share) value = share; //We can't buy more tokens than available
-				    
-				    sum[0] -= value; //update the total balance of the pool
-				    tokensToApply[i] -= value; //subtract the tokens from the member
-				}
-				
-				totalSent += value; //track how much was sent
-				sum[2] += value; //update the total balance of the pool
-				
-				moneyToApply[i] += value; //update the member's money
-				
-				if(!stage1 || amount-totalSent <= sum[0]){ //if we're at the last iteration of the main loop and we need to write the changes to storage
-				    if(tokensToApply[i] != debtPool.balance[member][0]){ //if we've removed any tokens of that person
-				        TokenTransfer(memberAddresses[msg.sender], member, pool, tokensToApply[i] - int256(debtPool.balance[member][0]), now); //send the event
-				    }
-				    if(moneyToApply[i] != debtPool.balance[member][2]){ //if this person's money have changed
-				        MoneyTransfer(memberAddresses[msg.sender], member, pool, moneyToApply[i] - int256(debtPool.balance[member][2]), now); //send the event
-				    }
-				    
-				    debtPool.balance[member][0] = uint128(tokensToApply[i]);
-				    debtPool.balance[member][2] = uint128(moneyToApply[i]);
-				}
-				
-				if(totalSent >= amount) break; //avoid useless looping if we're done
-    		}
-    		
-    		stage1 = false;
-	    }
-        
-        debtPool.totalBalance = sum;
-        
-		if(totalSent!=amount) throw; //if there was an error in the splitting algorithm
-		
-		TokenPurchase(memberAddresses[msg.sender], pool, totalSent, now);
+    function buyTokens(uint16 pool, uint128 amount) public only(MembersLib.PermLevel.admin) {
+        //pools[pool].buyTokens(pool, amount, memberAddresses[msg.sender]);
     }
 }
